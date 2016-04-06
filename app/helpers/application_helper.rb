ed99348ea96653c62c8047cd6e388ff237be7e27
Hassan Akram <hassan.akram3282@gmail.com>
2016-04-06 17:32:43 +0500

    Fixed layout issue in code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def highlight_code(text)
     text.gsub(/\<pre\>(.+?)\<code( class="language-(.+?)")?\>(.+?)\<\/code\>\<\/pre\>/m) do
-      CodeRay.scan($4, $3).div(line_numbers: :table)
+      CodeRay.scan($4, $3).div
     end
   end
 end
