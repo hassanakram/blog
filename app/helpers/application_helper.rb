15b1003952e797fe28400f0ab5c046e5759de691
Hassan Akram <hassan.akram3282@gmail.com>
2016-04-13 22:02:15 +0500

    Replcaed html codes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def highlight_code(text)
-    text.gsub(/\<pre\>(.+?)\<code( class="language-(.+?)")?\>(.+?)\<\/code\>\<\/pre\>/m) do
+    CGI.unescapeHTML(text).gsub(/\<pre\>(.+?)\<code( class="language-(.+?)")?\>(.+?)\<\/code\>\<\/pre\>/m) do
       CodeRay.scan($4, $3).div
     end
   end
